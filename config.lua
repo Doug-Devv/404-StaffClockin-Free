3edb1176dcdb0b69018667fad090e186f3be0d31
Douggie 🐱 <okrzesikbankston@gmail.com>
2025-05-21 15:43:40 -0500

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,19 @@
+Config = {}
+
+-- Your Discord webhook URL
+Config.WebhookURL = "your.webhook.url"
+
+-- Webhook avatar image
+Config.WebhookAvatar = ""  -- Webhook profile image
+
+-- Embed image (displays in the message body)
+Config.EmbedImage = ""
+
+-- Webhook username
+Config.WebhookName = "404 Free Staff Clockin System"
+
+-- Discord embed color deecimal format
+Config.EmbedColor = 3447003  -- Blue
+
+-- Footer text
+Config.FooterText = "Clockin Logs Made By 404 Solutions"
